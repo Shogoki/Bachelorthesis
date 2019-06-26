2b28a0ba2dca269a8700b2544998f88caa4f735e
Sven Kraus <mail@svenkraus.de>
2019-06-26 22:54:59 +0200

    adjusted pipeline a bit

--- upload_thesis.py
+++ upload_thesis.py
@@ -1,5 +1,7 @@
 import owncloud
 import sys
+import os  
+
 if len(sys.argv) < 2:
 	print("Please specify password")
 	quit()
@@ -12,3 +14,6 @@ oc.login('azure-pl', pw)
 # oc.mkdir('testdir')
 
 oc.put_file('output/thesis_latest.pdf', 'output/thesis.pdf')
+##uploading wordcount if present
+if os.path.isfile("plot_wordcount.png"):
+	oc.put_file('plot_wordcount.png', 'output/plot_wordcount.png')
